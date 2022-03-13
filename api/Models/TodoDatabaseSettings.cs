ffbb62787c1e0a9b500f3fbc629c4aed022cca0c
Mike van Egmond <m.vanegmond@student.han.nl>
2022-03-13 16:13:16 +0100

    Removed code for swagger / placeholder stuff

--- api/Models/TodoDatabaseSettings.cs
+++ api/Models/TodoDatabaseSettings.cs
@@ -3,8 +3,6 @@
 public class TodoDatabaseSettings
 {
     public string ConnectionString { get; set; } = null!;
-
     public string DatabaseName { get; set; } = null!;
-
     public string TodosCollectionName { get; set; } = null!;
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.EntityFrameworkCore;
 using api.Models;
 using api.Services;
 using Microsoft.Net.Http.Headers;
@@ -6,28 +5,16 @@ using Microsoft.Net.Http.Headers;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers()
     .AddJsonOptions(
-        options => options.JsonSerializerOptions.PropertyNamingPolicy = null); // Dit zorgt ervoor dat de hoofdletters worden behouden aan het begin van woorden.
+        options => options.JsonSerializerOptions.PropertyNamingPolicy =
+            null); // Dit zorgt ervoor dat de hoofdletters worden behouden aan het begin van woorden.
 
-builder.Services.Configure<TodoDatabaseSettings>(
-    builder.Configuration.GetSection("TodoDatabase"));
+builder.Services.Configure<TodoDatabaseSettings>(builder.Configuration.GetSection("TodoDatabase"));
 
 builder.Services.AddSingleton<TodoService>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
